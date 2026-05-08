a5bb3bbbfe3a910fd57fd502c6587f63b45ef140
grsoliveira <goliveira@olixcorp.com>
2026-05-08 19:50:00 -0300

    feat: modificando tipo de retorno para json

--- server.go
+++ server.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Cotacao struct {
+	Bid string `json:bid`
+}
+
 func main() {
 	http.HandleFunc("/cotacao", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Servidor rodando!")
+		cotacao := Cotacao{Bid: "2.81"}
+
+		writer.Header().Set("Content-type", "application/json")
+		json.NewEncoder(writer).Encode(cotacao)
 	})
 	fmt.Println("Servidor rodando na porta 8080")
 	http.ListenAndServe(":8080", nil)
