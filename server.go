8387e443712a0c61df5e010b0caa31156805572d
grsoliveira <goliveira@olixcorp.com>
2026-05-08 19:45:36 -0300

    feat: criação da primeira versão do servidor. corrigindo o pacote para main

--- server.go
+++ server.go
@@ -1,1 +1,14 @@
-package _1_go_client_server
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/cotacao", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Servidor rodando!")
+	})
+	fmt.Println("Servidor rodando na porta 8080")
+	http.ListenAndServe(":8080", nil)
+}
